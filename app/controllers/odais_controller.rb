28873276593a6ed9268b916e1a6652ffe2b932e4
baku_nishiya <niya-baku@users.noreply.github.com>
2018-11-04 23:04:05 +0900

    odai.allを実装

--- app/controllers/odais_controller.rb
+++ app/controllers/odais_controller.rb
@@ -1,6 +1,7 @@
 class OdaisController < ApplicationController
   before_action :authenticate_user!, only: :authentication
   def index
+    @odais = Odai.all.order(created_at: :desc) #投稿時に降順に表示される
   end
 
   def authentication
