735248a3f5f8ee77e2428d278acf123e64e5da8a
baku_nishiya <niya-baku@users.noreply.github.com>
2018-11-04 23:47:49 +0900

    sign_up機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
   get 'odais/index'
   devise_for :users
-  get 'posts/index'
+  #get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
